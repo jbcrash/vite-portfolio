746ee509b8c9f3a396c67209414a8d9958343ac1
Josh Bellingham <108699936+jbcrash@users.noreply.github.com>
2023-10-13 16:40:12 -0600

    Finalizing changes for final build Version 1

--- src/components/navigation/SocialLinksData.tsx
+++ src/components/navigation/SocialLinksData.tsx
@@ -4,27 +4,27 @@ import '../../styles.css'
 export const SocialLinksData = [
     {
         title: 'LinkedIn',
-        path: 'https://www.linkedin.com/in/josh-bellingham-7627a7239/',
+        path: 'https://www.linkedin.com/in/josh-bellingham/',
         icon: <FaIcons.FaLinkedin className='grow' alt='LinkedIn' />
     },
-    {
-        title: 'Behance',
-        path: 'https://www.behance.net/joshbellingham1',
-        icon: <FaIcons.FaBehanceSquare className='grow' alt='Behance' />
-    },
-    {
-        title: 'Dribbble',
-        path: 'https://dribbble.com/jbcrash?onboarding=true',
-        icon: <FaIcons.FaDribbbleSquare className='grow' alt='Dribbble'/>
-    },
-    {
-        title: 'Fiverr',
-        path: 'https://www.fiverr.com/joshbellingham1?up_rollout=true',
-        icon: <FaIcons.FaFeatherAlt className='grow' alt='Fiverr'/>
-    },
+    // {
+    //     title: 'Behance',
+    //     path: 'https://www.behance.net/joshbellingham1',
+    //     icon: <FaIcons.FaBehanceSquare className='grow' alt='Behance' />
+    // },
+    // {
+    //     title: 'Dribbble',
+    //     path: 'https://dribbble.com/jbcrash?onboarding=true',
+    //     icon: <FaIcons.FaDribbbleSquare className='grow' alt='Dribbble'/>
+    // },
+    // {
+    //     title: 'Fiverr',
+    //     path: 'https://www.fiverr.com/joshbellingham1?up_rollout=true',
+    //     icon: <FaIcons.FaFeatherAlt className='grow' alt='Fiverr'/>
+    // },
     {
         title: 'GitHub',
-        path: 'https://www.fiverr.com/joshbellingham1?up_rollout=true',
+        path: 'https://github.com/jbcrash',
         icon: <FaIcons.FaGithubSquare className='grow' alt='Github'/>
     }
 ]
