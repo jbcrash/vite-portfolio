a18028a4a97a342414a1db4caa62b968c39099ce
Josh Bellingham <108699936+jbcrash@users.noreply.github.com>
2024-06-19 15:08:38 -0600

    Updating resume

--- src/components/navigation/SocialLinksData.tsx
+++ src/components/navigation/SocialLinksData.tsx
@@ -5,16 +5,16 @@ export const SocialLinksData = [
     {
         title: 'LinkedIn',
         path: 'https://www.linkedin.com/in/josh-bellingham/',
-        icon: <FaIcons.FaLinkedin className='grow'/>
+        icon: <FaIcons.FaLinkedin className='grow' title='LinkedIn' />
     },
     {
         title: 'Credly',
         path: 'https://www.credly.com/users/joshua-bellingham',
-        icon: <FaIcons.FaCertificate className='grow'/>
+        icon: <FaIcons.FaCertificate className='grow' title='Credly'/>
     },
     {
         title: 'GitHub',
         path: 'https://github.com/jbcrash',
-        icon: <FaIcons.FaGithubSquare className='grow'/>
+        icon: <FaIcons.FaGithubSquare className='grow' title='GitHub'/>
     }
 ]
